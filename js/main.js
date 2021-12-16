c7f1fff99710c334d15abde612acd55f0f051f31
Maury Tejada <maurytejada94@gmail.com>
2021-12-16 12:44:15 -0300

    submenu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
-addEventListener('DOMContentLoaded', () => {
-    const btn_menu = document.querySelector('.btn-menu');
-    if(btn_menu){
-        btn_menu-addEventListener('click', () =>{
-            const menu_items = document.querySelector('.menu-items')
-            menu_items.classList.toggle('mostrar')
-        })
-    }  
-})
-
+function openMenu(element){
+    console.log(element);
+    document.getElementsByClassName("menu-items")[0].classList.toggle("unhide");
+}
 function openSubmenu(element){
     console.log(element);
     document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide");
