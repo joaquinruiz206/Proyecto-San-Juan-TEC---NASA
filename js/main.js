ae886e467571f42b44e5a805a63f098e53d78454
Maury Tejada <maurytejada94@gmail.com>
2021-12-16 13:02:18 -0300

    submenu nuevo

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ function openSubmenu(element){
     console.log(element);
     document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide");
 }
+
+function openSubmenuPlanet(element){
+    console.log(element);
+    document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide-Planets");
+}
