95d5e676ef82a3995cd1a64ef3842a5bab209551
Maury Tejada <maurytejada94@gmail.com>
2021-12-16 12:17:26 -0300

    header responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-function openMenu(element){
-    console.log(element);
-    document.getElementsByTagName("nav")[0].classList.toggle("nav-disable");
-}
+addEventListener('DOMContentLoaded', () => {
+    const btn_menu = document.querySelector('.btn-menu');
+    if(btn_menu){
+        btn_menu-addEventListener('click', () =>{
+            const menu_items = document.querySelector('.menu-items')
+            menu_items.classList.toggle('mostrar')
+        })
+    }  
+})
 
 function openSubmenu(element){
     console.log(element);
