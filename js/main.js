fe3219bc278d83e3d694ecba8221b4c455554754
Maury Tejada <maurytejada94@gmail.com>
2021-12-17 14:50:03 -0300

    nav terminado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-function openMenu(element){
-    console.log(element);
-    document.getElementsByClassName("menu-items")[0].classList.toggle("unhide");
-}
 function openSubmenu(element){
     console.log(element);
     document.getElementsByClassName("submenu")[0].classList.toggle("submenuUnhide");
